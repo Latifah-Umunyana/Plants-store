63b4b7bbbad2dbcd581554a7dba982957cd09dcc
Latifa-Umunyana <latifahumunyana@gmail.com>
2024-04-05 19:52:57 +0300

    add file

--- control-assignment.js/control-Assignment-file.js
+++ control-assignment.js/control-Assignment-file.js
@@ -32,7 +32,7 @@ return newArray;
 
 }
 
-// console.log(changeNumbers(numbersArray));
+console.log(changeNumbers(numbersArray));
 
 // Another way for question1
 
@@ -62,7 +62,7 @@ return nums;
 
 }
 
-// console.log(changeNumbers1({numbersArray2}));
+console.log(changeNumbers1({numbersArray2}));
     
 // Write a program that takes in the following array and use a while loop to iterate and break when the
 //  item is equal to the fourth index :let arrNum = [1,2,3,4,5,6,7,8,9];
